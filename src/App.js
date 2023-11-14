c380691b22838b82e11f69bfca6e359d745ee488
Michael Monson <michaelmonson@gmail.com>
2023-11-14 06:18:23 +0000

    Updated our 'Square' component to use state.   - Handles a click now, and sets the value to 'X' by using the state value.   - Because we repeat the square 9 times for each square in the board, each has their own independent state.   - Removed the default value from each Square component, and all squares start out with a default value of 'null'   - Now clicking in each square will cause an 'X' to appear because we calling the state setter (setValue('X')) instead of console logging it as we were before.

--- src/App.js
+++ src/App.js
@@ -1,27 +1,35 @@
+import { useState } from 'react';
+
 /* Create a custom React Component called 'Square' to pass into our Board component.
  * I didn't realize you could have multiple components defined in the same file!
  */
-function Square({ value }) {
-  return <button className="square">{value}</button>;
+function Square() {
+  const [value, setValue] = useState(null);
+  
+  function handleClick() {
+    setValue('X');
+  }
+
+  return <button className="square" onClick={handleClick}>{value}</button>;
 }
 
 export default function Board() {
   return (
     <>
       <div className="board-row">
-        <Square value="1" />
-        <Square value="2" />
-        <Square value="3" />
+        <Square />
+        <Square />
+        <Square />
       </div>
       <div className="board-row">
-        <Square value="4" />
-        <Square value="5" />
-        <Square value="6" />
+        <Square />
+        <Square />
+        <Square />
       </div>
       <div className="board-row">
-        <Square value="7" />
-        <Square value="8" />
-        <Square value="9" />
+        <Square />
+        <Square />
+        <Square />
       </div>
     </>
   );
