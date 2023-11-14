6d3e2926a9a75a0066d00923e0ba848a443e7b38
Michael Monson <michaelmonson@gmail.com>
2023-11-14 05:45:11 +0000

    Broke each "Square" into it's own component.

--- src/App.js
+++ src/App.js
@@ -1,20 +1,27 @@
-export default function Square() {
+/* Create a custom React Component called 'Square' to pass into our Board component.
+ * I didn't realize you could have multiple components defined in the same file!
+ */
+function Square({ value }) {
+  return <button className="square">{value}</button>;
+}
+
+export default function Board() {
   return (
     <>
       <div className="board-row">
-        <button className="square">1</button>
-        <button className="square">2</button>
-        <button className="square">3</button>
+        <Square value="1" />
+        <Square value="2" />
+        <Square value="3" />
       </div>
       <div className="board-row">
-        <button className="square">4</button>
-        <button className="square">X</button>
-        <button className="square">6</button>
+        <Square value="4" />
+        <Square value="5" />
+        <Square value="6" />
       </div>
       <div className="board-row">
-        <button className="square">7</button>
-        <button className="square">8</button>
-        <button className="square">9</button>
+        <Square value="7" />
+        <Square value="8" />
+        <Square value="9" />
       </div>
     </>
   );
